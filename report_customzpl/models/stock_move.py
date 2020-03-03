1c500e909d8f076f5e9c87120d0d5abf1669fa1f
wissensc <48807761+wissensc@users.noreply.github.com>
2020-03-03 10:22:17 -0600

    actualizar
    actualizar

--- report_customzpl/models/stock_move.py
+++ report_customzpl/models/stock_move.py
@@ -3,5 +3,5 @@ from odoo import models, fields, api, _
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
-    w_num_imp = fields.Integer('Cantidad de impresiones', required=False)
+    _inherit = 'stock.move.line'
+    w_num_imp = fields.Integer('Impresiones', required=False)
